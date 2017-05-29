de899fb12f5f6eea798cef224d7783854a08e25e
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-05-29 13:58:26 +0800

    edited banner, added links.php

---
+++ links.php
@@ -0,0 +1,95 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>Links</title>
+	  <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+	  <div id="app">
+  		<?php include('html-includes/navigation.html'); ?>
+      <section class="landing__banner-bar">
+        <div class="container">
+          <h1>Links</h1>
+          <ol class="breadcrumb">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Links</a>
+            </li>
+          </ol>
+        </div>
+      </section>
+
+      <section>
+        <div class="standard-content">
+  				<h2>Organisers</h2>
+          <ul>
+            <li>
+              <a href="http://www.rafflesmarina.com.sg/" target="_blank">Raffles Marina</a>
+              <img alt="Brand" width="1.5%" src="http://www.westerncircuit.com/images/RML Logo_HiReso.png">
+            </li>
+    				<li>
+              <a href="http://www.smu.edu.sg/" target="_blank">Singapore Management University</a>
+              <img alt="Brand" width="1.2%" src="http://www.westerncircuit.com/images/SMU-logo-2-2.png">
+            </li>
+    				<li>
+              <a href="http://www.smusailing.com/" target="_blank">SMU Sailing Club</a>
+            </li>
+          </ul>
+
+  				<h2>Singapore Sailing Federation</h2>
+          <ul>
+            <li>
+              <a href="http://www.sailing.org.sg/" target="_blank">Singapore Sailing Federation</a>
+            </li>
+          </ul>
+
+  				<h2>Regattas</h2>
+          <ul>
+            <li>
+              <a href="http://straitsregatta.com/home.php" target="_blank">Singapore Straits Regatta</a>
+            </li>
+            <li>
+              <a href=" http://www.sailing.org.sg/events/regatta.php" target="_blank">Singapore Sailing Calendar</a>
+            </li>
+            <li>
+              <a href="http://asiapacificstudentcup.com" target="_blank">Asia Pacific Student Cup</a>
+            </li>
+          </ul>
+
+  				<h2>Useful Information</h2>
+          <ul>
+            <li>
+              <a href="http://irc-sea.com/revalidation-online/" target="_blank">IRC Revalidation </a>
+            </li>
+            <li>
+              <a href="http://www.sailing.org/rrs" target="_blank">ISAF Racing Rules of Sailing</a>
+            </li>
+          </ul>
+
+  				<h2>Weather & Tides</h2>
+          <ul>
+            <li>
+              <a href="http://app.nea.gov.sg/cms/htdocs/category_sub.asp?cid=231" target="_blank">Weather Information</a>
+            </li>
+            <li>
+              <a href="http://app2.nea.gov.sg/tidetime.aspx" target="_blank">Tide Information</a>
+            </li>
+            <li>
+              <a href="http://app.nea.gov.sg/cms/htdocs/category_sub.asp?cid=231" target="_blank">Wind Information</a>
+            </li>
+          </ul>
+
+
+
+        </div>
+      </section>
+
+  		<?php include('html-includes/footer.html'); ?>
+	  </div>
+	  <!-- Scripts -->
+	  <script src="dist/app.js"></script>
+  </body>
+</html>
