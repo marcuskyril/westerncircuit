c5317a1c97c43c3af67497c57bf477f1ef8475db
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-05-31 01:03:33 +0800

    created an assets folder, created entry-list.php

---
+++ entry-list.php
@@ -0,0 +1,62 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>Entry List</title>
+	  <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+	  <div id="app">
+  		<?php include('html-includes/navigation.html'); ?>
+      <section class="landing__banner-bar">
+        <div class="container">
+          <h1>Entry List</h1>
+          <ol class="breadcrumb">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">Entry List</a>
+            </li>
+          </ol>
+        </div>
+      </section>
+
+      <section>
+        <div class="standard-content">
+  				<table width="100%">
+            <tbody>
+              <?php
+
+              // $json_string = file_get_contents("./assets/entry-list.json");
+              // $json = json_decode($json_string, true);
+              //
+              // $sailID = 'Sail 123';
+              // $skipperName = 'John Mayer';
+              // $yachtName = 'Search For Everything';
+              //
+              // $test = array('sailID' => $sailID, 'yachtName' => $yachtName, 'skipperName' => $skipperName);
+              // array_push($json['irc A'], $test);
+              // $edited_json = json_encode($json);
+              // file_put_contents('./assets/entry-list.json', $edited_json);
+
+              $json_string = file_get_contents("./assets/entry-list.json");
+              $json = json_decode($json_string, true);
+              foreach ($json as $class => $entries) {
+                echo '<tr><th colSpan=3>',$class,'</th></tr>';
+                foreach ($entries as $entry) {
+                  echo '<tr><td>'.$entry['sailID'].'</td><td>'.$entry['yachtName'].'</td><td>'.$entry['skipperName'].'</td></tr>';
+                }
+              }
+              ?>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+  		<?php include('html-includes/footer.html'); ?>
+	  </div>
+	  <!-- Scripts -->
+	  <script src="dist/app.js"></script>
+  </body>
+</html>
