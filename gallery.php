483f7e3618b24a1b4c73a0a97a3b686189f4004f
Joshua <joshymantou@gmail.com>
2017-06-01 03:04:30 +0800

    Removed multiple redundant gallery pages. Iframes now in gallery.php. Aesthetics need to be improved

--- gallery.php
+++ gallery.php
@@ -23,8 +23,9 @@
       </section>
 
       <section>
+<!--
         <div class="standard-content">
-            <!-- gallery links here-->
+             gallery links here
             
             <div class="list-group">
               <a href="gallery/gallery_race_1_howie.php" class="list-group-item list-group-item-action">Race Day 1 - Photos by Howie Choo (Saturday, 13th August 2016)</a>
@@ -44,13 +45,107 @@
                 <a href="gallery/gallery_race_3_howie.php" class="list-group-item list-group-item-action">Race Day 3 - Photos by Howie Choo (Saturday, 20th August 2016)</a>
                 <a href="gallery/gallery_race_3_spencer.php" class="list-group-item list-group-item-action">Race Day 3 - Photos by Spencer (Saturday, 20th August 2016)</a>
             </div>
+            
         </div>
+-->
+          
+          
+      <div class="standard-content">
+
+          <div class="list-group">
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_1">Race Day 1 - Photos by Howie Choo (Saturday, 13th August 2016)</a>
+                </div>
+              
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_2">Race Day 1 - Photos by Spencer (Saturday, 13th August 2016)</a>
+                </div>    
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 1 - Photos by Victor Lim (Saturday, 13th August 2016)</a>
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 1 - Photos by Stanley Chee (Saturday, 13th August 2016)</a>
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 2 - Photos by Howie Choo (Sunday, 14th August 2016)</a>
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 2 - Photos by Victor Lim (Sunday, 14th August 2016)</a>
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 2 - Photos by Stanley Chee (Sunday, 14th August 2016)</a>
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 2 - Photos by William Ong (Sunday, 14th August 2016)</a> 
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 3 - Photos by Howie Choo (Saturday, 20th August 2016)</a> 
+                </div>
+                <div class="list-group-item">
+                    <a href="#" class="links" id="link_3">Race Day 3 - Photos by Spencer (Saturday, 20th August 2016)</a> 
+                </div>
+          </div>
+
+
+          
+          <!-- iframes-->
+          <div id="race-day-1-howie" class="race"> 
+                <iframe id="race-1-howie" src="//embedsocial.com/facebook_album/album_photos/710787272401657" width="100%" height="1500px" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-1-spencer" class="race"> 
+                <iframe src="//embedsocial.com/facebook_album/album_photos/711141202366264" width="100%" height="1500px" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-1-victor" class="race"> 
+                <iframe src="//embedsocial.com/facebook_album/album_photos/710795979067453" width="100%" height="1500px" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-1-stanley" class="race"> 
+                <iframe src="//embedsocial.com/facebook_album/album_photos/710792845734433" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-2-howie" class="race"> 
+               <iframe src="//embedsocial.com/facebook_album/album_photos/711675312312853" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-2-victor" class="race"> 
+                <iframe src="//embedsocial.com/facebook_album/album_photos/710986009048450" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-2-stanley" class="race"> 
+                <iframe src="//embedsocial.com/facebook_album/album_photos/710987639048287" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-2-william" class="race">
+                <iframe src="//embedsocial.com/facebook_album/album_photos/710983365715381" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          
+          <div id="race-day-3-howie" class="race">
+                <iframe src="//embedsocial.com/facebook_album/album_photos/714722702008114" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe> 
+          </div>
+
+          <div id="race-day-3-spencer" class="race">
+                <iframe src="//embedsocial.com/facebook_album/album_photos/714728758674175" width="100%" height="1500" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+          </div>
+          <!-- iframes-->
+          
+      </div>
         
       </section>
 
-  		<?php include('html-includes/footer.html'); ?>
+        <?php include('html-includes/footer.html'); ?>
 	  </div>
 	  <!-- Scripts -->
 	  <script src="dist/app.js"></script>
+      
+      <!-- Gallery Render js-->
+      <script>
+            $('a.links').click(function (e){
+                e.preventDefault();
+            var div_id = $('a.links').index($(this))
+                $('.race').hide().eq(div_id).show();
+            });      
+      </script>
   </body>
 </html>
