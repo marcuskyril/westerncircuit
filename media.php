a21ecc0a806f69ba450cda0cfee2a7f532c3572e
Joshua <joshymantou@gmail.com>
2017-05-31 15:50:16 +0800

    Media.php added, together with pdf files

---
+++ media.php
@@ -0,0 +1,72 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <title>Documentation</title>
+	  <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+	  <div id="app">
+  		<?php include('html-includes/navigation.html'); ?>
+      <section class="landing__banner-bar">
+        <div class="container">
+          <h1>Media</h1>
+          <ol class="breadcrumb">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">Media</a>
+            </li>
+          </ol>
+        </div>
+      </section>
+
+      <section>
+        <div class="standard-content">
+  				<table width="100%">
+            <tbody>
+              <tr>
+                <th>
+                  Document
+                </th>
+                <th>
+                  Download(s)
+                </th>
+              </tr>
+              <tr>
+                <td>
+                  Western Bugle Day 1
+                </td>
+                <td>
+                  <a href="pdf/19th_WCSR_Newsletter_Day1.pdf"><i class="fa fa-file-pdf-o"></i></a>
+                </td>
+              </tr>
+              <tr>
+                 <td>
+                  Western Bugle Day 2
+                </td>
+                <td>
+                  <a href="pdf/19th_WCSR_Newsletter_Day2.pdf"><i class="fa fa-file-pdf-o"></i></a>
+                </td>
+              </tr>
+              <tr>
+                 <td>
+                  Western Bugle Day 3
+                </td>
+                <td>
+                  <a href="pdf/19th_WCSR_Newsletter_Day3.pdf"><i class="fa fa-file-pdf-o"></i></a>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+  		<?php include('html-includes/footer.html'); ?>
+	  </div>
+	  <!-- Scripts -->
+	  <script src="dist/app.js"></script>
+  </body>
+</html>
