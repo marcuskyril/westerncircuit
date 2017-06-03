a60c5ccdb7dc52752251eb7ecc08987ed2d52403
Joshua <joshymantou@gmail.com>
2017-06-03 18:02:16 +0800

    File upload function

--- dashboard.php
+++ dashboard.php
@@ -4,12 +4,16 @@
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <title>Dashboard</title>
 	  <link rel="stylesheet" href="dist/app.css">
+      <link href="src/scss/libraries/jquery.fileuploader.scss" media="all" rel="stylesheet">
   </head>
   <body>
     <?php
       // do some fancy password protect shit
       // firebase..?
     ?>
+    
+      
+      
 	  <div id="app">
   		<?php include('html-includes/navigation.html'); ?>
       <section class="landing__banner-bar">
@@ -63,13 +67,173 @@
               <a href="#" class="btn">Submit</a>
               <div id="uploadResult"></div>
             </form>
+            <div class="fileuploader fileuploader-theme-dragdrop">
+                <form id="uploadForm" action="upload.php" method="POST" enctype="multipart/form-data">
+                    <input type="file" name="files">
+                    <input type="submit" class="btn">
+                </form>
+            </div>
+              
+            <p id="msg"></p>  
           </section>
         </div>
       </main>
 
   		<?php include('html-includes/footer.html'); ?>
+        
+        
 	  </div>
 	  <!-- Scripts -->
 	  <script src="dist/app.js"></script>
+      <script src="/src/js/jquery.fileuploader.min.js" type="text/javascript"></script>
+      <!-- File Uploading client sideJS-->
+      <script type="text/javascript">
+        $('input[name="files"]').fileuploader({
+            	// enable thumbnails for files {null, Object}
+	// set on null to disable the thumbnails
+	thumbnails: {
+		// thumbnails list HTML {String, Function}
+		// example: '<ul></ul>'
+		// example: function(options) { return '<ul></ul>'; }
+		box: '<div class="fileuploader-items">' +
+			  '<ul class="fileuploader-items-list"></ul>' +
+		      '</div>',
+		
+		// append thumbnails list to selector {null, String, jQuery Object}
+		// example: 'body'
+		// example: $('body')
+		boxAppendTo: null,
+		
+		// thumbnails item HTML {String, Function}
+		// example: '<li>${name}</li>'
+		// example: function(item) { return '<li>' + item.name + '</li>'; }
+		item: '<li class="fileuploader-item">' +
+			   '<div class="columns">' +
+			   	'<div class="column-thumbnail">${image}</div>' +
+				'<div class="column-title">' +
+				     '<div title="${name}">${name}</div>' +
+				     '<span>${size2}</span>' +
+				'</div>' +
+				'<div class="column-actions">' +
+				     '<a class="fileuploader-action fileuploader-action-remove" title="Remove"><i></i></a>' +
+				'</div>' +
+			    '</div>' +
+			    '<div class="progress-bar2">${progressBar}<span></span></div>' +
+			'</li>',
+		
+		// thumbnails appended item HTML {String, Function}
+		// example: '<li>${name}</li>'
+		// example: function(item) { return '<li>' + item.name + '</li>'; }
+		item2: '<li class="fileuploader-item">' +
+			   '<div class="columns">' +
+			   	'<a href="${data.url}" target="_blank">' +
+				     '<div class="column-thumbnail">${image}</div>' +
+				     '<div class="column-title">' +
+				          '<div title="${name}">${name}</div>' +
+				          '<span>${size2}</span>' +
+				     '</div>' +
+				'</a>' +
+				'<div class="column-actions">' +
+				     '<a href="${file}" class="fileuploader-action fileuploader-action-download" title="Download" download><i></i></a>' +
+				     '<a class="fileuploader-action fileuploader-action-remove" title="Remove"><i></i></a>' +
+				'</div>' +
+			    '</div>' +
+			'</li>',
+			
+		// insert the thumbnail's item at the beginning of the list? {Boolean}
+		itemPrepend: false,
+		
+		// show a confirmation dialog by removing a file? {Boolean}
+		// it will not be shown in upload mode by canceling an upload
+		removeConfirmation: true,
+		
+		// render the image thumbnail? {Boolean}
+		// if it will be false, it will generate an icon(you can also hide it with css)
+		// if it will be false, you can use the API method (item.renderImage()) to render it (check thumbnails example)
+		startImageRenderer: true,
+		
+		// render the images synchron {Boolean}
+		// made to improve the browser speed
+		synchronImages: true,
+		
+		// render the image in a canvas element {Boolea, Object}
+		// if it will be true, it will generate an image with the css sizes from the parent element (.column-thumbnail)
+		// you can also set the width and the height in the object
+		// by default - true
+		canvasImage: {
+			width: null,
+			height: null
+		},
+		
+		// thumbnails selectors
+		_selectors: {
+			list: '.fileuploader-items-list',
+			item: '.fileuploader-item',
+			start: '.fileuploader-action-start',
+			retry: '.fileuploader-action-retry',
+			remove: '.fileuploader-action-remove'
+		},
+		
+		// Callback fired before adding the list element
+		beforeShow: function(parentEl, newInputEl, inputEl) {
+			// callback will go here
+		},
+		
+		// Callback fired after adding the item element
+		onItemShow: function(item, listEl, parentEl, newInputEl, inputEl) {
+			// callback will go here
+		},
+		
+		// Callback fired after removing the item element
+		// by default we will animate the removing action
+		onItemRemove: function(itemEl, listEl, parentEl, newInputEl, inputEl) {
+			itemEl.children().animate({'opacity': 0}, 200, function() {
+				setTimeout(function() {
+					itemEl.slideUp(200, function() {
+						itemEl.remove(); 
+					});
+				}, 100);
+			});
+		},
+		
+		// Callback fired after the item image was loaded
+		onImageLoaded: function(itemEl, listEl, parentEl, newInputEl, inputEl) {
+			// callback will go here
+		},
+	},
+    
+   
+            
+    
+    // enable the drag&drop feature {Boolean, Object}
+	// this feature is available only in upload mode
+	// by default - true
+	dragDrop: {
+		// set drag&drop container {null, String, jQuery Object}
+		// example: 'body'
+		// example: $('body')
+		container: null,
+		
+		// Callback fired on entering with dragged files the drop container
+		onDragEnter: function(event, listEl, parentEl, newInputEl, inputEl) {
+			// callback will go here
+            console.log("callback", event);
+		},
+		
+		// Callback fired on leaving with dragged files the drop container
+		onDragLeave: function(event, listEl, parentEl, newInputEl, inputEl) {
+			// callback will go here
+		},
+		
+		// Callback fired on dropping the dragged files in drop container
+		onDrop: function(event, listEl, parentEl, newInputEl, inputEl) {
+			// callback will go here
+		},
+
+	},
+
+        });
+      </script>
+        
   </body>
 </html>
