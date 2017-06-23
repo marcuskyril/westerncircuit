0f1e6d8063f3e2e398cc507265bd980908cafd8a
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-06-23 23:00:03 +0800

    ready for final vet

--- public/assets/dist/app.js
+++ public/assets/dist/app.js
@@ -13978,24 +13978,6 @@ countdown.init();
 
 var dashboard = {
   init: function init() {
-    // $('#documentType').on('change', function(e) {
-    //   var docType = $('.nice-select .current')[1].innerHTML.toLowerCase();
-    //   console.log(docType);
-    //   $('select').niceSelect('update');
-    //
-    // });
-    //
-    // $('#documentation-list').on('change', function(e) {
-    //   var docName = $('.nice-select .current')[2].innerHTML.toLowerCase();
-    //   console.log(docName);
-    //   $('select').niceSelect('update');
-    // });
-    //
-    // $('#media-list').on('change', function(e) {
-    //   var mediaName = $('.nice-select .current')[3].innerHTML.toLowerCase();
-    //   console.log(mediaName);
-    //   $('select').niceSelect('update');
-    // });
 
     $('.doc-toggle a').click(function (e) {
       $('.doc-toggle a').removeClass('active');
