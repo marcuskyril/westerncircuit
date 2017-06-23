6689d5ee49c2ed5c798399bfc2019d3d26d6f8fc
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-06-23 19:08:30 +0800

    completed dashboard

--- public/assets/src/js/dashboard.js
+++ public/assets/src/js/dashboard.js
@@ -1,39 +1,20 @@
 let dashboard = {
   init() {
-    $('#documentType').on('change', function(e) {
-      var docType = $('.nice-select .current')[1].innerHTML.toLowerCase();
-      console.log(docType);
-      $('select').niceSelect('update');
-
-    });
-
-    $('#documentation-list').on('change', function(e) {
-      var docName = $('.nice-select .current')[2].innerHTML.toLowerCase();
-      console.log(docName);
-      $('select').niceSelect('update');
-    });
-
-    $('#media-list').on('change', function(e) {
-      var mediaName = $('.nice-select .current')[3].innerHTML.toLowerCase();
-      console.log(mediaName);
-      $('select').niceSelect('update');
-    });
 
     $('.doc-toggle a').click(function(e) {
       $('.doc-toggle a').removeClass('active');
       $(this).addClass('active');
-      $('#doc-upload-form .nice-select').toggle();
+      $('#doc-upload-form .nice-select-container').toggle();
       $('#doc-upload-form input[name="documentName"]').toggle();
     });
 
     $('.media-toggle a').click(function(e) {
       $('.media-toggle a').removeClass('active');
       $(this).addClass('active');
-      $('#media-upload-form .nice-select').toggle();
+      $('#media-upload-form .nice-select-container').toggle();
       $('#media-upload-form input[name="mediaName"]').toggle();
     });
   }
-
 }
 
 dashboard.init();
