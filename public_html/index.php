db5d72e6ae6f39921e6f51c1b47597570726573d
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-06-25 17:53:38 +0800

    edited router

--- public_html/index.php
+++ public_html/index.php
@@ -1,27 +1,33 @@
 <?php
-// Grabs the URI and breaks it apart in case we have querystring stuff
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
-
-// print_r ($request_uri);
 
 $routes = array(
-                "/" => "home",
-                "/contact" => "contact",
-                "/crew-search" => "crew-search",
-                "/dashboard" => "dashboard",
-                "/documentation" => "documentation",
-                "/entry-list" => "entry-list",
-                "/gallery" => "gallery",
-                "/links" => "links",
-                "/login" => "login",
-                "/media" => "media",
-                "/results" => "results",
-                "/schedule" => "schedule",
-                "/sponsors" => "sponsors"
-              );
+  "/" => "home",
+  "/contact" => "contact",
+  "/crew-search" => "crew-search",
+  "/dashboard" => "dashboard",
+  "/documentation" => "documentation",
+  "/entry-list" => "entry-list",
+  "/gallery" => "gallery",
+  "/links" => "links",
+  "/login" => "login",
+  "/media" => "media",
+  "/results" => "results",
+  "/schedule" => "schedule",
+  "/sponsors" => "sponsors"
+);
 
-if(isset($routes[$request_uri[0]])) {
-  require '../app/view/'.$routes[$request_uri[0]].'.php';
+function getCurrentUri() {
+  $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+  $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+  if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+  $uri = '/' . trim($uri, '/');
+  return $uri;
+}
+
+$base_url = getCurrentUri();
+
+if(isset($routes[$base_url])) {
+  require '../app/view/'.$routes[$base_url].'.php';
 } else {
   header('HTTP/1.0 404 Not Found');
   require '../app/view/404.php';
